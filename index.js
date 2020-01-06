adf30623504c8fa10fb72019cf9e172c7ea6722f
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-01-06 16:55:25 +0700

    [GCI-wit03] fix code from suggestion

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//home endpoint
 app.get('/', (req, res) => {
     res.send('Go to /info for more information');
 })
 
+//info endpoint
 app.get('/info', (req, res) => {
     res.json({ 
         serverName: serverInfo.name, 
         serverVersion: serverInfo.version 
     })
 })
-
 
 app.listen(port, () => console.log(`listening on port ${port}!`)) 
